302c19b9e8ab90d7ebe0419432fd9a8d405aa6d0
ASTiNEZh <buzhenitsavladik@gmail.com>
2024-07-12 15:29:11 +0300

    first commit

---
+++ src/test/java/ru/astinezh/audience/IventManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.astinezh.audience;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IventManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
